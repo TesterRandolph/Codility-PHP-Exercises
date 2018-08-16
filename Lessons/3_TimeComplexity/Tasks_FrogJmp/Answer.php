6dd3ad029481d7650e417e91ba1d584cf90b015f
TesterRandolph <yichiunchang@gmail.com>
2018-08-16 16:34:59 +0800

    Fixed the function parameter of the task FronJmp.

--- Lessons/3_TimeComplexity/Tasks_FrogJmp/Answer.php
+++ Lessons/3_TimeComplexity/Tasks_FrogJmp/Answer.php
@@ -2,7 +2,7 @@
 // you can write to stdout for debugging purposes, e.g.
 // print "this is a debug message\n";
 
-function solution($A, $K) {
+function solution($X, $Y, $D) {
     // write your code in PHP7.0
     $min = 1;
     $max = 1000000000;
