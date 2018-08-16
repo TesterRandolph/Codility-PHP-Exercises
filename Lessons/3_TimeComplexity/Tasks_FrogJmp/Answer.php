44ecbafaf7d17b8cd1f2ab056efd5758ce447dee
TesterRandolph <yichiunchang@gmail.com>
2018-08-16 16:25:13 +0800

    Awesome Completed for FrogJmp of Lesson_3_TimeComplexity!!

---
+++ Lessons/3_TimeComplexity/Tasks_FrogJmp/Answer.php
@@ -0,0 +1,25 @@
+<?php
+// you can write to stdout for debugging purposes, e.g.
+// print "this is a debug message\n";
+
+function solution($A, $K) {
+    // write your code in PHP7.0
+    $min = 1;
+    $max = 1000000000;
+
+    if ($X < $min || $X > $max) return 0;
+
+    if ($Y < $min || $Y > $max) return 0;
+
+    if ($D < $min || $D > $max) return 0;
+
+    if ($X > $Y) return 0;
+
+    $tmp = ($Y - $X) % $D;
+
+    $result = ($Y - $X - $tmp) / $D;
+
+    if ($tmp > 0) $result += 1;
+
+    return $result;
+}
