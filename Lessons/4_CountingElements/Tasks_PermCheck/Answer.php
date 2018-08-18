faed021c4401ddf4f5ce9c288cc9360d1151cb87
TesterRandolph <yichiunchang@gmail.com>
2018-08-18 20:34:44 +0800

    Result_1 for PermCheck of Lesson_4_CountingElements.

---
+++ Lessons/4_CountingElements/Tasks_PermCheck/Answer.php
@@ -0,0 +1,42 @@
+<?php
+// you can write to stdout for debugging purposes, e.g.
+// print "this is a debug message\n";
+
+function solution($A) {
+    // write your code in PHP7.0
+    $tmpArray = [];
+    $arrLength = sizeof($A);
+
+    if ($arrLength < 1 || $arrLength > 100000) return 0;
+
+    if ($arrLength === 1) return 1;
+
+    $min = $A[0];
+    $max = $A[0];
+
+    for ($i = 0; $i < $arrLength; $i++)
+    {
+        if ($A[$i] < 1 || $A[$i] > 1000000000) return 0;
+
+        if ($i === 0)
+        {
+            $min = $A[$i];
+            $max = $A[$i];
+        }
+
+        if ($A[$i] < $min) $min = $A[$i];
+
+        if ($A[$i] > $max) $max = $A[$i];
+
+        $tmpArray[$A[$i]][] = $i;
+
+        if (sizeof($tmpArray[$A[$i]]) !== 1) return 0;
+    }
+
+    for ($i = $min; $i <= $max; $i++)
+    {
+        if (!isset($tmpArray[$i])) return 0;
+    }
+
+    return 1;
+}
