ddd203a4a909f1ce71b133156936a7bff593f6d0
TesterRandolph <yichiunchang@gmail.com>
2018-08-21 12:54:42 +0800

    Awesome Completed for MinAvgTwoSlice of Lesson_5_PrefixSums.

--- Lessons/5_PrefixSums/Tasks_MinAvgTwoSlice/Answer.php
+++ Lessons/5_PrefixSums/Tasks_MinAvgTwoSlice/Answer.php
@@ -16,12 +16,16 @@ function solution($A) {
     {
         if ($A[$i] < -10000 || $A[$i] > 10000) return 0;
 
-        if (!isset($A[$i])) break;
+        if (!isset($A[$i + 1])) break;
+
+        if ($A[$i + 1] < -10000 || $A[$i + 1] > 10000) return 0;
 
         if ($i === 0)
         {
             $start = 0;
             $min = ($A[$i] + $A[$i + 1]) / 2;
+
+            continue;
         }
 
         $tmp = ($A[$i] + $A[$i + 1]) / 2;
@@ -30,6 +34,19 @@ function solution($A) {
         {
             $start = $i;
             $min = $tmp;
+        }
+
+        if (isset($A[$i + 2]))
+        {
+            if ($A[$i + 2] < -10000 || $A[$i + 2] > 10000) return 0;
+
+            $tmp = ($A[$i] + $A[$i + 1] + $A[$i + 2]) / 3;
+
+            if ($tmp < $min)
+            {
+                $start = $i;
+                $min = $tmp;
+            }
         }
     }
 
