488beaf248559b06f33c087508d87d96ce2aa536
TesterRandolph <yichiunchang@gmail.com>
2018-08-25 23:43:19 +0800

    Result_5 for Triangle of Lesson_6_Sorting.

--- Lessons/6_Sorting/Tasks_Triangle/Answer.php
+++ Lessons/6_Sorting/Tasks_Triangle/Answer.php
@@ -6,34 +6,40 @@ function solution($A) {
     // write your code in PHP7.0
     $arrLength = sizeof($A);
 
+    $tmpArray = [];
+
     if ($arrLength < 0 || $arrLength > 100000) return 0;
 
     for ($l = 0; $l < $arrLength; $l++)
     {
         if ($A[$l] < -2147483648 || $A[$l] > 2147483648) return 0;
+
+        if ($A[$l] > 0) $tmpArray[] = [$l, $A[$l]];
     }
 
-    for ($i = 0; $i < $arrLength - 2; $i++)
+    $tmpArrLength = sizeof($tmpArray);
+
+    for ($i = 0; $i < $tmpArrLength - 2; $i++)
     {
         $j = $i + 1;
         $separate = 1;
 
-        $limit = $arrLength - $j;
+        $limit = $tmpArrLength - $j;
 
         while ($separate < $limit)
         {
             $nextJ = $j + $separate;
 
-            if ($A[$i] + $A[$j] > $A[$nextJ] &&
-                $A[$i] + $A[$nextJ] > $A[$j] &&
-                $A[$j] + $A[$nextJ] > $A[$i])
+            if ($tmpArray[$i][1] + $tmpArray[$j][1] > $tmpArray[$nextJ][1] &&
+                $tmpArray[$i][1] + $tmpArray[$nextJ][1] > $tmpArray[$j][1] &&
+                $tmpArray[$j][1] + $tmpArray[$nextJ][1] > $tmpArray[$i][1])
             {
                 return 1;
             }
 
             $j++;
 
-            if ($nextJ === $arrLength - 1)
+            if ($nextJ === $tmpArrLength - 1)
             {
                 $j = $i + 1;
                 $separate++;
