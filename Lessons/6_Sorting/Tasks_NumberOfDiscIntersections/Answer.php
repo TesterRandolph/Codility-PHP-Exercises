5f3ce5c51ae5d49f7f4b37b43a7d6f0e1ae71214
TesterRandolph <yichiunchang@gmail.com>
2018-08-27 00:26:21 +0800

    Result_2 for NumberOfDiscIntersections of Lesson_6_Sorting.

--- Lessons/6_Sorting/Tasks_NumberOfDiscIntersections/Answer.php
+++ Lessons/6_Sorting/Tasks_NumberOfDiscIntersections/Answer.php
@@ -10,66 +10,36 @@ function solution($A) {
     // if ($arrLength === 0 || $arrLength === 1) return 0;
     if ($arrLength <= 1 || $arrLength > 100000) return 0;
 
-    $circles = [];
-    $points = [];
-
+    $maxIndex = $arrLength - 1;
     $intersect = 0;
 
     for ($i = 0; $i < $arrLength; $i++)
     {
         if ($A[$i] < 0 || $A[$i] > 2147483647) return 0;
 
-        if ($A[$i] === 0)
+        $leftLimit = $i - $A[$i];
+        $rightLimit = $i + $A[$i];
+
+        if ($rightLimit > $maxIndex)
         {
-            $points[] = $i;
+            $intersect += $arrLength - $i + 1;
+
+            if ($intersect > 10000000) return -1;
+
             continue;
         }
 
-        $circles[] = [$i, $i - $A[$i], $i + $A[$i]];
-    }
-
-    $circlesLength = sizeof($circles);
-    $pointsLength = sizeof($points);
-    
-    // circles to circles
-    if ($circlesLength > 1)
-    {
-        $jLimit = $circlesLength - 1;
-
-        for ($j = 0; $j < $jLimit; $j++)
+        for ($j = $rightLimit + 1; $j < $arrLength; $j++)
         {
-            for ($k = $j + 1; $k < $circlesLength; $k++)
-            {
-                if ($circles[$j][1] > $circles[$k][2] ||
-                    $circles[$j][2] < $circles[$k][1])
-                {
-                    continue;
-                }
+            $jLeft = $j - $A[$j];
 
-                $intersect++;
-            }
-        }
-    }
+            if ($leftLimit > $jLeft || $rightLimit < $jLeft) continue;
 
-    // circles to points
-    if ($circlesLength > 0 && $pointsLength > 0)
-    {
-        for ($j = 0; $j < $circlesLength; $j++)
-        {
-            for ($k = 0; $k < $pointsLength; $k++)
-            {
-                if ($circles[$j][1] > $points[$k] ||
-                    $circles[$j][2] < $points[$k])
-                {
-                    continue;
-                }
+            $intersect++;
 
-                $intersect++;
-            }
+            if ($intersect > 10000000) return -1;
         }
     }
     
-    if ($intersect > 10000000) return -1;
-
     return $intersect;
 }
