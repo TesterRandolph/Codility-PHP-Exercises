2848c2a3618d650deb3d388739d28ec7a0e4108c
TesterRandolph <yichiunchang@gmail.com>
2018-09-03 17:25:28 +0800

    Result_1 for MaxProfit of Lesson_9_MaximumSliceProblem.

---
+++ Lessons/9_MaximumSliceProblem/Tasks_MaxProfit/Answer.php
@@ -0,0 +1,56 @@
+<?php
+// you can write to stdout for debugging purposes, e.g.
+// print "this is a debug message\n";
+
+function solution($A) {
+    // write your code in PHP7.0
+    $arrLength = sizeof($A);
+
+    if ($arrLength < 0 || $arrLength > 400000) return 0;
+
+    if ($arrLength < 1) return 0;
+
+    if ($arrLength === 2) return $A[1] - $A[0];
+
+    $tmpArray = [];
+    $tmpLength = 0;
+
+    $iLimit = $arrLength - 1;
+
+    $negativeCount = 0;
+
+    for ($i = 0; $i < $iLimit; $i++)
+    {
+        $next = $i + 1;
+        $profit = $A[$next] - $A[$i];
+        $tmpArray[$tmpLength] = [$i, $next, $profit];
+        $tmpLength++;
+
+        if ($profit < 0)
+        {
+            $negativeCount++;
+        }
+    }
+
+    if ($negativeCount === $iLimit) return 0;
+
+    $max = 0;
+
+    for ($j = 0; $j < $tmpLength; $j++)
+    {
+        if ($tmpArray[$j][2] < 0) continue;
+
+        $tmpProfit = 0;
+
+        for ($k = $j; $k < $tmpLength; $k++)
+        {
+            if ($tmpProfit + $tmpArray[$k][2] < 0) break;
+
+            $tmpProfit += $tmpArray[$k][2];
+
+            if ($tmpProfit > $max) $max = $tmpProfit;
+        }
+    }
+
+    return $max;
+}
